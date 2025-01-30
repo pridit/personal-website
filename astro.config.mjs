2d5578d0781a2ba06170a479d8230b54e30da5e7
Jordan Heale <webmaster@pridit.co.uk>
2025-01-30 19:47:04 +0000

    feat: replace deprecated tailwindcss package, v4 fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.pridit.co.uk",
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
   output: "static",
   vite: {
+    plugins: [tailwindcss()],
     server: {
       watch: {
         usePolling: true,

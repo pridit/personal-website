ea29448267f9e6d442d814e2df57bbd7cb2c66da
Jordan Heale <webmaster@pridit.co.uk>
2025-12-14 23:20:22 +0000

    chore(deps): add eslint & prettier, formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.pridit.co.uk",
+  site: 'https://www.pridit.co.uk',
   integrations: [sitemap()],
-  output: "static",
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
     server: {
